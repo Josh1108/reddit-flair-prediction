7bae01a0df36fa2acc6f20d8b852b9a427c4d42e
Jushaan Kalra <Jushaan18@gmail.com>
2019-09-10 17:33:05 +0530

    initial commit

---
+++ Data_aquisition(2).py
@@ -0,0 +1,34 @@
+import praw
+import pandas as pd
+
+reddit = praw.Reddit(client_id='KDMo4mS_QD1eqw', client_secret='RPKj_1mVLobhY7IBbFhHiu0nY4o', user_agent='WebScrapper') # Using credentials to use Reddit API
+Indian_reddit =reddit.subreddit('India') # Subreddit in reddit
+
+
+
+get_flairs = Indian_reddit.search("AskIndia", limit=200)
+posts = []
+flair="AskIndia"
+for submission in get_flairs:
+    submission.comments.replace_more(limit=0)
+    comments=''
+    for comment in submission.comments.list():
+        comments = ' '+comments + comment.body
+    posts.append([submission.title, submission.score, submission.id, submission.url, submission.num_comments, submission.selftext, submission.over_18,submission.author,comments,flair])
+data = pd.DataFrame(posts,columns=['title', 'score', 'id', 'url', 'num_comments', 'body', 'over_18','author','comments','flair'])
+
+flairs_except_1 = ["Non-Political", "[R]eddiquette", "Scheduled", "Photography", "Science/Technology", "Politics", "Business/Finance", "Policy/Economy", "Sports", "Food", "AMA"]
+
+for flair in flairs_except_1:
+    get_flairs = Indian_reddit.search(flair, limit=1000)
+    posts = []
+    for submission in get_flairs:
+        submission.comments.replace_more(limit=0)
+        comments=''
+        for comment in submission.comments.list():
+            comments = ' '+comments + comment.body
+        posts.append([submission.title, submission.score, submission.id, submission.url, submission.num_comments, submission.selftext, submission.over_18,submission.author,comments,flair])
+    data_temp = pd.DataFrame(posts,columns=['title', 'score', 'id', 'url', 'num_comments', 'body', 'over_18','author','comments','flair'])
+    data=pd.concat([data,data_temp], sort =False)
+    print(flair)
+data.to_csv('./Reddit-ML-model(2).csv', index=False)
---
+++ application/script.py
@@ -0,0 +1,91 @@
+#importing libraries
+import os
+import numpy as np
+import flask
+import pickle
+from flask import Flask, render_template,request
+import praw
+import nltk
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import make_pipeline
+from nltk.corpus import stopwords
+import re
+import contractions
+
+
+wnl = WordNetLemmatizer()
+remove =set(stopwords.words('english'))
+def edit(words):
+    words =str(words)
+    words=re.sub('([.,////])',' ',words)
+    words=re.sub('\[.*?\]', '', words)
+    words = words.replace('\n', ' ')
+    words = contractions.fix(words)
+    word_list = nltk.word_tokenize(re.sub(r'([^a-z A-Z])', '', words.lower()))
+    comment = ' '.join([wnl.lemmatize(w) for w in word_list if w not in remove])
+    return comment
+
+
+def edit2(words):
+    words=str(words)
+    if 'https://www.reddit.com/r/india/comments/' in words:
+        count =0
+        s=''
+        for i in range(len(words)):
+            if words[i]=='/':
+                count+=1
+            elif count==7:
+                s+=words[i]
+        lis = s.split('_')
+        t=' '.join(word for word in lis )
+        return str(t)
+    else:
+        return words
+
+def Praw_data(URL):
+    reddit = praw.Reddit(client_id='KDMo4mS_QD1eqw', client_secret='RPKj_1mVLobhY7IBbFhHiu0nY4o', user_agent='WebScrapper') # Using credentials to use Reddit API
+    Info = reddit.submission(url=URL)
+    posts = []
+    Info.comments.replace_more(limit=0)
+    comments=''
+    for comment in Info.comments.list():
+        comments = ' '+comments + comment.body
+    posts.extend([Info.title, comments, Info.selftext, Info.url,Info.link_flair_text])
+    posts[0]=edit(posts[0])
+    posts[1]=edit(posts[1])
+    posts[2]=edit(posts[2])
+    posts[3]=edit2(posts[3])
+    s=''
+    s=posts[0]+' '+posts[1]+' '+posts[2]+' '+posts[3]
+    print(s)
+    return (s,posts[4])
+
+
+
+
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/flair',methods=['POST'])
+def flair():
+    URL=request.form['GetURL']
+    combined_str, flair = Praw_data(URL)
+    loaded_model = pickle.load(open("../model(2).pkl","rb"))
+    result=loaded_model.predict([combined_str])
+    result=result[0]
+    return render_template('flair.html',result = str(result),flair=flair)
+
+
+@app.route('/statistics')
+def stats():
+    return render_template('statistics.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
